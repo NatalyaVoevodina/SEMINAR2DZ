04fe2424a040cbfffc7096eb7132a4ba98b7a8ff
natlya_voevodina <voevodochka1988@yandex.ru>
2023-02-18 17:28:25 +0300

    task 2.2

--- task2.1/Program.cs
+++ task2.1/Program.cs
@@ -3,12 +3,10 @@
 Console.WriteLine("Введите номер дня недели"); // вводится число в промежутке от 1 до 7
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num>=6)
-{
-Console.WriteLine("Выходной день");
-}
-   else 
-{
-Console.WriteLine("Будний день");
-
-}
+if(num == 0 || num > 7) 
+  Console.WriteLine("Введите число от 1 до 7");
+    else if(num >= 1 && num <= 5)
+    Console.WriteLine("Рабочий день");
+    else if(num == 6 || num == 7)
+   Console.WriteLine("Выходной день");
+   
